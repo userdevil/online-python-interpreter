ea7d2050b62abb88e7d85855fb4f4f1681aa5064
Mavin Sandeep <62639000+userdevil@users.noreply.github.com>
2022-12-18 16:08:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ def home():
     return render_template('index.html')
 
 if __name__=='__main__':
-    app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True)
